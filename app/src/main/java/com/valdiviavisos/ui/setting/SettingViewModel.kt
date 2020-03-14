279e5a3c551d1947669fcb60bfc413a06ca017e7
maxes <maxespi@gmail.com>
2020-03-14 19:02:07 -0300

    Initial commit

---
+++ app/src/main/java/com/valdiviavisos/ui/setting/SettingViewModel.kt
@@ -0,0 +1,13 @@
+package com.valdiviavisos.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
